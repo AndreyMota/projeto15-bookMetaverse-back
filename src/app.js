43abcec4b72ba3bfbae0353170853c95935aabbe
TamaraFerr <tamarablacksmith@hotmail.com>
2023-08-31 19:01:27 -0300

    feat: folders ready

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import router from './routes/index.routes.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(json());
+app.use(router);
+
+app.listen(PORT, () => console.log(`--------------- Server running on port ${PORT}`));
