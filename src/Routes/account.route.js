20f351b61b2816196f3b53b7f50167e138d96758
TamaraFerr <tamarablacksmith@hotmail.com>
2023-09-05 00:30:05 -0300

    feat: userInfo

--- src/Routes/account.route.js
+++ src/Routes/account.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import validateAuth from "../middlewares/validateAuth.js";
-import { signUp, signIn, logout, EditUser } from "../Controllers/account.controller.js";
+import { signUp, signIn, logout, EditUser, UserInfo} from "../Controllers/account.controller.js";
 import validateSchema from "../middlewares/validateSchema.js";
 import { SignInSchema, SignUpSchema, EditUserSchema } from "../Schemas/account.schema.js";
 
@@ -9,6 +9,7 @@ const accountRouter = Router();
 accountRouter.post('/cadastro', validateSchema(SignUpSchema), signUp);
 accountRouter.post('/login', validateSchema(SignInSchema), signIn);
 accountRouter.post('/logout', validateAuth, logout);
+accountRouter.get('/info-usuario', UserInfo);
 accountRouter.post('/editar-usuario', validateSchema(EditUserSchema), EditUser)
 
 export default accountRouter;
